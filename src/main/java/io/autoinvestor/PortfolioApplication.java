9c84d5c5e2d382b441689f1e1d6a420d5a847ec2
Pol Pinol Castuera <pol.pinol@mongodb.com>
2025-03-17 19:33:53 +0100

    Introduce CI pipeline and an initial structure

--- src/main/java/io/autoinvestor/PortfolioApplication.java
+++ src/main/java/io/autoinvestor/PortfolioApplication.java
@@ -1,4 +1,4 @@
-package io.autoinvestor.portfolio;
+package io.autoinvestor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
 }
--- src/test/java/io/autoinvestor/PortfolioApplicationTests.java
+++ src/test/java/io/autoinvestor/PortfolioApplicationTests.java
@@ -1,4 +1,4 @@
-package io.autoinvestor.portfolio;
+package io.autoinvestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PortfolioApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
