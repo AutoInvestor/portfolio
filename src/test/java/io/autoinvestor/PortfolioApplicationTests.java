c78a634c4bc13d377e627d9714ebe17f6e42ef83
AlfredoMate <mate.alfredo98@gmail.com>
2025-05-21 21:12:30 +0200

    solvingTest

--- src/test/java/io/autoinvestor/PortfolioApplicationTests.java
+++ src/test/java/io/autoinvestor/PortfolioApplicationTests.java
@@ -3,7 +3,7 @@ package io.autoinvestor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PortfolioApplicationTests {
 
     @Test
